0f92ca77cc7e86f8e2e150963c8eebf722e9e006
liuwei03 <love544560370@qq.com>
2022-12-06 16:50:56 +0800

    添加日志

--- src/main/java/org/example/MySqlCDC22.java
+++ src/main/java/org/example/MySqlCDC22.java
@@ -21,6 +21,7 @@ public class MySqlCDC22 {
         Map<String, Object> customConverterConfigs = new HashMap<>();
         customConverterConfigs.put(JsonConverterConfig.DECIMAL_FORMAT_CONFIG, "numeric");
 
+        // 监听Mysql
         MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
                 .hostname("hadoop1")
                 .port(3306)
@@ -32,12 +33,15 @@ public class MySqlCDC22 {
                 .jdbcProperties(properties)
                 .build();
 
+        // 创建执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        // 创建检查点
         env.enableCheckpointing(3000);
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
         env.getCheckpointConfig().setCheckpointStorage("file:///Users/lw/project/bdyx_project/FlinkCDC/src/main/resources");
 
+        // 执行 打印到控制台
         env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
            .setParallelism(4)
            .print().setParallelism(1);
